da2695a13b1d7f23119324ad1624c42f2d3f400d
BK_bakerevil <92763889+bakerevil@users.noreply.github.com>
2024-06-11 18:21:43 -0600

    tabla company

---
+++ database/migrations/2024_06_11_235450_create_table_company.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableCompany extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('company', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 80);
+            $table->string('mail', 255);
+            $table->string('phone', 10);
+            $table->string('contact', 255);
+            $table->string('logo', 255);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('company');
+    }
+}
